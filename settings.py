d11a78acc3e17fc45bf887204635ceadb13a8ab7
abosen <abosen@qq.com>
2018-08-16 23:21:27 +0800

    update some description
    add support of leetcode search

--- settings.py
+++ settings.py
@@ -1,9 +1,11 @@
 # -*- coding:utf-8 -*-
 
-src_path = "your/source/path"
-url_pattern = "https://leetcode-cn.com/problems/%s/description/"
+src_path = "your/src/path"
+search_limit = 10
 
-table_pattern = '''### {title}
+search_display = "{index:0>3s} {level} {percent} {ch_name}"
+
+md_pattern = '''### {title}
 |\t|\t|
 |---:|:---|
 |题号|{index:0>3s}|
@@ -46,3 +48,7 @@ public class SolutionTest {{
     }}
 }}
 '''
+
+detail_url = "https://leetcode-cn.com/problems/%s/description/"
+
+query_url = "https://leetcode-cn.com/problems/api/filter-questions/%s"
