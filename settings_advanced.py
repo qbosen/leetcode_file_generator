2555d87777b73780043525feb2041b3255620261
qbosen <abosen@qq.com>
2018-08-20 12:27:29 +0800

    add advanced settings

---
+++ settings_advanced.py
@@ -0,0 +1,15 @@
+query_keys = [
+    'questionId',
+    'questionTitle',
+    'translatedTitle',
+    # 'translatedContent',
+    'difficulty',
+    'stats',
+    'codeDefinition',
+    'sampleTestCase',
+    r'topicTags{name\n translatedName}'
+]
+
+debug_mode = True
+language = 'java'
+
